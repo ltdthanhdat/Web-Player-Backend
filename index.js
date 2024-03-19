f7e2c8f95f4d4ee34d63b38c41681ca9f0ca7a35
thanhdat <lethanhdat.real@gmail.com>
2024-03-19 23:42:06 +0700

    update mvc pattern

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const express = require('express')
+import express from 'express'
+import 'dotenv/config'
+import connectDB from './connectDB.js'
+import songRoutes from './routes/songRoutes.js'
+import playlistRoutes from './routes/playlistRoutes.js'
+
 const app = express()
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('hello worfald')
-})
 
-app.listen(3000)
+// connect db
+const MONGO_URI = process.env.MONGO_URI
+connectDB(MONGO_URI)
+
+
+// middle ware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+// routing
+app.use('/songs', songRoutes)
+app.use('/playlists', playlistRoutes)
+
+
+// port listening
+app.listen(PORT)
+console.log(`Listening to PORT ${PORT}`)
